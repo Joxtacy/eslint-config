e78a0fb64f6496e48669de8f813cf6a8e59042a1
Jesper Hasselquist <jesper.hasselquist@telavox.se>
2021-02-01 14:02:35 +0900

    Add ESLint rules

---
+++ index.js
@@ -0,0 +1,86 @@
+module.exports = {
+    extends: ["eslint:recommended"],
+    rules: {
+        "eol-last": ["error", "always"],
+        "indent": [
+            "error",
+            4,
+            {
+                "SwitchCase": 1
+            }
+        ],
+        "key-spacing": [
+            "warn",
+            {
+                "beforeColon": false,
+                "afterColon": true,
+                "mode": "strict"
+            }
+        ],
+        "keyword-spacing": "error",
+        "linebreak-style": ["error", "unix"],
+        "no-console": [
+            "error",
+            {
+                "allow": [
+                    "warn",
+                    "info",
+                    "group",
+                    "groupEnd"
+                ]
+            }
+        ],
+        "no-multiple-empty-lines": [
+            "warn",
+            {
+                "max": 2,
+                "maxEOF": 1,
+                "maxBOF": 0
+            }
+        ],
+        "no-negated-condition": ["warn"],
+        "no-tabs": "warn",
+        "no-trailing-spaces": [
+            "warn",
+            {
+                "skipBlankLines": false,
+                "ignoreComments": false
+            }
+        ],
+        "no-useless-concat": "error",
+        "no-var": "error",
+        "object-curly-spacing": [
+            "warn",
+            "always",
+            {
+                "arraysInObjects": false,
+                "objectsInObjects": false
+            }
+        ],
+        "prefer-const": [
+            "error",
+            {
+                "destructuring": "any",
+                "ignoreReadBeforeAssign": false
+            }
+        ],
+        "prefer-template": "warn",
+        "quotes": [
+            "error",
+            "double",
+            {
+                "avoidEscape": true
+            }
+        ],
+        "semi": ["error", "always"],
+        "space-before-blocks": "error",
+        "switch-colon-spacing": [
+            "error",
+            {
+                "after": true,
+                "before": false
+            }
+        ],
+        "template-curly-spacing": ["warn", "never"]
+    }
+}
